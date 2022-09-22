21b3f4f870f1b71e2f3d4a7fea9af15c24a397d2
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-09-22 10:36:13 -0300

    :pizza:

--- app/components/pedidos/index.js
+++ app/components/pedidos/index.js
@@ -4,16 +4,22 @@ function tamanhoPizza(tamanho) {
     let borda = document.querySelector('body > main > div > div.informacoes-pizza > div > div.fundo-pizza > div');
     let queijo = document.querySelector('body > main > div > div.informacoes-pizza > div > div.fundo-pizza > div > div');
 
-
+    queijo.innerText = ""
 
     if (tamanho == 1) {
         borda.style = "width: 150px; height: 150px;"
         queijo.style = "width: 130px; height: 130px;"
+        queijo.style.backgroundColor = "rgb(163, 132, 53)"
+        borda.style.backgroundColor = "rgb(213, 163, 103)"
     } else if (tamanho == 2) {
         borda.style = "width: 200px; height: 200px;"
         queijo.style = "width: 180px; height: 180px;"
+        queijo.style.backgroundColor = "rgb(163, 132, 53)"
+        borda.style.backgroundColor = "rgb(213, 163, 103)"
     } else if (tamanho == 3) {
         borda.style = "width: 250px; height: 250px;"
         queijo.style = "width: 225px; height: 225px;"
+        queijo.style.backgroundColor = "rgb(163, 132, 53)"
+        borda.style.backgroundColor = "rgb(213, 163, 103)"
     }
 }
